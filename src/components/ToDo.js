445bccddc182c4358929cd8565889174bda70175
webdevbrent <webdeveloperbrent@gmail.com>
2018-01-30 22:45:51 -0500

    Ability to add todos added

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,8 +4,9 @@ class ToDo extends Component {
   render() {
     return (
       <li>
-        <input type="checkbox" checked={ this.props.isCompleted } />
-        <span>{ this.props.description }</span>
+        <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+        <span>{ this.props.description } {''}</span>
+        <button onClick={() => { this.deleteTodo }}>Delete</button>
       </li>
     );
   }
